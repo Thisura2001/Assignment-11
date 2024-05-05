4505ef9f65d888179567ff5b7d5790d06120bd0c
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-05-05 21:13:44 +0530

    table listener done

--- Controller/CustomerController.js
+++ Controller/CustomerController.js
@@ -1,14 +1,15 @@
 import CustomerModel from "../Model/CustomerModel.js";
 import { Customers } from "../Db/Db.js";
 
+let customerTableClicked;
 function loadCustomerTable() {
     $("#CustomerTableBody").empty();
     Customers.forEach((item) => {
         var record = `<tr>
-            <td>${item.customerId}</td>
-            <td>${item.customerName}</td>
-            <td>${item.salary}</td>
-            <td>${item.address}</td>
+            <th scope="row" class="cusId">${item.customerId}</th>
+            <td class="cusName">${item.customerName}</td>
+            <td class="cusSalary">${item.salary}</td>
+            <td class="cusAddress">${item.address}</td>
         </tr>`;
         $("#CustomerTableBody").append(record);
     });
@@ -24,3 +25,17 @@ $("#btnCustomerSave").on('click', function () {
     Customers.push(CustomerObj);
     loadCustomerTable();
 });
+$("#CustomerTableBody").on('click','tr',function (){
+    let index = $(this).index();
+    customerTableClicked = index;
+
+    let customerId = $(this).find(".cusId").text();
+    let customerName = $(this).find(".cusName").text();
+    let salary = $(this).find(".cusSalary").text();
+    let address = $(this).find(".cusAddress").text();
+
+    $("#cus_id").val(customerId);
+    $("#name").val(customerName);
+    $("#salary").val(salary);
+    $("#address").val(address);
+})
