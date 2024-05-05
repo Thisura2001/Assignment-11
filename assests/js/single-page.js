824045c2e38e047d6288fcd484f75ea23dc62049
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-05-05 14:42:24 +0530

    Initial commit

---
+++ assests/js/single-page.js
@@ -0,0 +1,25 @@
+$("#nav-home").on('click',function (){
+    $("#home").css({display:'block'})
+    $("#customer").css({display:'none'})
+    $("#items").css({display:'none'})
+    $("#orders").css({display:'none'})
+});
+
+$("#nav-customer").on('click',function (){
+    $("#home").css({display:'none'})
+    $("#customer").css({display:'block'})
+    $("#items").css({display:'none'})
+    $("#orders").css({display:'none'})
+});
+$("#nav-item").on('click',function (){
+    $("#home").css({display:'none'})
+    $("#customer").css({display:'none'})
+    $("#items").css({display:'block'})
+    $("#orders").css({display:'none'})
+});
+$("#nav-orders").on('click',function (){
+    $("#home").css({display:'none'})
+    $("#customer").css({display:'none'})
+    $("#items").css({display:'none'})
+    $("#orders").css({display:'block'})
+});
