dca5c15d0df0d106f1ee28f589c57fdd3ca28864
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-05-06 16:10:48 +0530

    item Table listener done

--- Controller/ItemController.js
+++ Controller/ItemController.js
@@ -1,19 +1,34 @@
 import { Items } from "../Db/Db.js";
 import ItemModel from "../Model/ItemModel.js";
 
+let itemTableClick;
 function loadItemTable() {
     $("#itemTableBody").empty();
     Items.forEach((item) => {
         var record = `<tr>
-            <td>${item.itemCode}</td>
-            <td>${item.itemName}</td>
-            <td>${item.price}</td>
-            <td>${item.qty}</td>
+            <td class="itemCode">${item.itemCode}</td>
+            <td class="NameItem">${item.itemName}</td>
+            <td class="itemPrice">${item.price}</td>
+            <td class="itemQty">${item.qty}</td>
         </tr>`;
         $("#itemTableBody").append(record);
     });
 }
+$("#itemTableBody").on('click','tr',function (){
+    let index = $(this).index();
+    itemTableClick = index;
 
+    let itemCode = $(this).find(".itemCode").text();
+    let itemName =$(this).find(".NameItem").text();
+    let price = $(this).find(".itemPrice").text();
+    let qty =$(this).find(".itemQty").text();
+
+    $("#Item_id").val(itemCode);
+    $("#item_Name").val(itemName);
+    $("#item_Price").val(price);
+    $("#itemQuantity").val(qty);
+
+})
 $("#btnItemSave").on('click', function () {
     let itemCode = $("#Item_id").val();
     let itemName = $("#item_Name").val();
