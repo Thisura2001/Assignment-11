9ab24f9ccb867a97477965dd06ae56932c6044e1
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-05-12 15:45:43 +0530

    place order comboBox done with some errors

--- Controller/placeOrderController.js
+++ Controller/placeOrderController.js
@@ -1,16 +1,13 @@
 import { Customers, Items } from "../Db/Db.js";
 
-// Function to clear and populate customer IDs in the combo box
+// Set the customer IDs in the combo box
 function updateCustomerIDs() {
-    // Clear existing options before adding new ones
     $('#selectCus_ID').empty();
-
-    // Add default option
     const defaultOption = document.createElement("option");
+
     defaultOption.text = "Select Customer Id";
     $('#selectCus_ID').append(defaultOption);
 
-    // Populate the combo box with customer IDs
     Customers.forEach(customer => {
         const option = document.createElement("option");
         option.value = JSON.stringify(customer);
