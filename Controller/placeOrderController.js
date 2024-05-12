924d2472a5db6848e3eba5a10eb98a0359ca669c
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-05-12 17:00:38 +0530

    place order comboBox done

--- Controller/placeOrderController.js
+++ Controller/placeOrderController.js
@@ -1,5 +1,6 @@
 import { Customers, Items } from "../Db/Db.js";
 
+let ItemIndexElement;
 // Set the customer IDs in the combo box
 function updateCustomerIDs() {
     $('#selectCus_ID').empty();
@@ -18,4 +19,39 @@ function updateCustomerIDs() {
 
 $('#selectCus_ID').on('focus', () => {
     updateCustomerIDs();
+});
+function updateItemIDs() {
+    $('#select').empty();
+    const defaultOption = document.createElement("option");
+
+    defaultOption.text = "Select Item ID";
+    $('#select').append(defaultOption);
+
+    Items.forEach(item => {
+        const option = document.createElement("option");
+        option.value = item.itemCode;
+        option.text = item.itemCode;
+        $('#select').append(option);
+    });
+}
+
+// Update item IDs when the dropdown receives focus
+$('#select').on('focus', () => {
+    updateItemIDs();
+});
+
+// Event listener for item selection change
+$('#select').on('change', () => {
+    const selectedOption = $('#select option:selected');
+
+    if (selectedOption.length > 0) {
+        const itemCode = selectedOption.val();
+        const selectedItem = Items.find(item => item.itemCode === itemCode);
+
+        $('#itemName').text(selectedItem.itemName);
+        $('#itemQut').text(selectedItem.qty);
+        $('#itemPrice').text(selectedItem.price);
+
+        $('#quantity_placeOrder').focus();
+    }
 });
