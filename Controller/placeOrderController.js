4c9732b0b206dbfc35a414c62ed58aace9e705e3
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-05-12 15:40:09 +0530

    place order comboBox done with some errors

--- Controller/placeOrderController.js
+++ Controller/placeOrderController.js
@@ -5,6 +5,11 @@ function updateCustomerIDs() {
     // Clear existing options before adding new ones
     $('#selectCus_ID').empty();
 
+    // Add default option
+    const defaultOption = document.createElement("option");
+    defaultOption.text = "Select Customer Id";
+    $('#selectCus_ID').append(defaultOption);
+
     // Populate the combo box with customer IDs
     Customers.forEach(customer => {
         const option = document.createElement("option");
@@ -14,7 +19,6 @@ function updateCustomerIDs() {
     });
 }
 
-// Attach event listener to the combo box when it receives focus
 $('#selectCus_ID').on('focus', () => {
     updateCustomerIDs();
 });
